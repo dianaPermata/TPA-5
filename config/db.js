3110bf3dab758c33c276aa195fe5730d3aa0fae8
dianaPermata <113364526+dianaPermata@users.noreply.github.com>
2022-11-14 15:28:18 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 const DB_URL = 'mongodb://localhost:27017/todo-list'
 
-// const DB_URL='mongodb+srv://diana9422120:diana9422120@cluster1.dclikdj.mongodb.net/todo-list'
-
 const db = mongoose.connect(DB_URL);
 
 module.exports = db;
